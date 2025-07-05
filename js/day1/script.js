76498850e1e5953e86b1560fd67e9341125714e0
Arun raj <113260000+Arun-cloud-dev@users.noreply.github.com>
2025-07-05 16:13:47 +0530

    update day  5 css

---
+++ js/day1/script.js
@@ -0,0 +1,14 @@
+console.log("Hello, World! welcome to ar s class with besaant tech ");
+
+
+
+
+const output = document.getElementById("output");
+
+
+
+document.write("Hello, World! welcome to ar s class with besaant tech <br>");
+
+
+output.innerHTML = "Hello, World! welcome to ar s class with besaant tech <br>";
+output.style.color = "red";
