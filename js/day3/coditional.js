9a8c9b4dfcd98a09a09a5029f5526cce188af8a1
Arun raj <113260000+Arun-cloud-dev@users.noreply.github.com>
2025-07-08 21:05:28 +0530

    day 3

---
+++ js/day3/coditional.js
@@ -0,0 +1,79 @@
+
+
+// let age = 100;
+
+
+
+// if (age >= 18) {
+//     console.log("You can vote this year");
+// } else {
+//     console.log("you can go to school to learn but not to vote ");
+// }
+
+
+
+
+
+
+
+// syntax 
+
+
+
+// if (condition) {
+//     //blopck to exicurte 
+// } else {
+    
+// }
+
+
+
+
+// let marks = 95;
+
+
+// if (marks >= 90) {
+//     console.log("Grade  A");
+// } else if (marks >= 75) {
+//     console.log("Grade B ");
+// } else{
+//     console.log("Grade C ");
+// }
+
+
+
+// switch  
+
+// switch (expression) {
+//     case 1:
+//         //code block 
+//         break;
+//     case 2:
+//         //code block
+//         break;
+    
+    
+//     default:
+//         //ciode block 
+// }
+
+
+let day = "Monday"
+
+switch (day) {
+  case "Monday":
+    console.log(" start the day with a smile ");
+    break;
+  case "Tuesday":
+    console.log(" start the day with a smile coz tuesady  ");
+    break;
+  case "Wednesday":
+    console.log(" start the day with a smile coz Wednesday ");
+    break;
+  case "Thursday":
+    console.log(" start the day with a smile coz Thursday ");
+        break;
+    default:
+        console.log("error in ypur day are alive coz yu are dead  ");
+    
+}
---
+++ js/day3/script.js
@@ -0,0 +1,85 @@
+// let  a = 5;
+// const b = 2;
+
+
+// console.log(++a);  //adddition
+
+
+
+// console.log(a - b);  //subtraction
+
+// console.log(a * b);  //multiplication
+// console.log(a / b);  //division
+// console.log(a % b);  //modulus reminder 
+
+
+
+//Assignment Operators
+
+
+
+// let x = 10;
+// let y = 5;
+
+// // +=   
+
+// x += 3
+
+// // x = x+3 
+
+// console.log(x);
+
+
+// x -= 2;
+
+
+
+// console.log(x);
+
+// x /= 3 
+
+
+// console.log(x);
+
+
+
+
+
+
+//comparison operators
+
+
+let a = 5;
+let b = 3;
+// 10 number 
+
+
+
+console.log(a == b); // true
+
+console.log(a === b);
+ 
+console.log(a != b);
+
+console.log(a !== b);
+
+console.log(a > b);
+
+console.log(a < b );
+
+console.log(a <= b);
+
+console.log(a >= b );
+
+
+
+//logical Operator 
+
+// &&(And)
+
+console.log(a > b && 5 < 4);
+
+
+console.log(!(a > b));
+
+
