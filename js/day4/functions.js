e68f179f91f845ac26188e8d7e8df7f88866f7fb
Arun raj <113260000+Arun-cloud-dev@users.noreply.github.com>
2025-07-09 21:38:40 +0530

    update

---
+++ js/day4/functions.js
@@ -0,0 +1,50 @@
+// function ar() {
+//     console.log("hello. world");
+// }
+
+// ar();
+
+
+// function add(z , y ) {
+//     return z  + y; 
+// }
+
+
+// let sum = add(5, 7)
+
+// console.log(sum);
+
+
+
+
+// const multyply = (z, y) => {
+//     return z * y
+// };
+
+// console.log(multyply(5, 5));
+
+
+// const square = m => m * m;
+// console.log(square(6));
+
+
+
+// function greet(name, callback) {
+//   console.log("Hi " + name);
+//   callback();
+// }
+
+// function sayBye() {
+//   console.log("Goodbye!");
+// }
+
+// greet("Arun", sayBye);
+  
+
+
+
+
+(function () {
+    alert("This is  immediate");
+})();
+
---
+++ js/day4/loops.js
@@ -0,0 +1,49 @@
+// for (let i = 0; i < 5; i++){
+//     console.log("number:" , i);
+// }
+
+
+
+
+// let i = 6;
+
+
+// while (i <= 5) {
+//     console.log("while loop:", i);
+//     i++;
+// }
+
+
+// let i = 1;
+
+// do {
+//     console.log("do-while", i);
+//     i++;
+// } while (i <= 0); 
+
+
+
+
+
+// const person = {
+//     name: "ar",
+//     age: "20",
+//     city:"chenai"
+// }
+
+
+// for (let key in person) {
+//     console.log(key, "- ", person[key]);
+    
+// }
+
+
+
+// const fruits = 'apple'
+
+
+// for (let fruit of fruits) {
+//     console.log(fruit);
+    
+// }
+
