8f188a92bb3e6babe361f5001039e82056947d4b
Arun raj <113260000+Arun-cloud-dev@users.noreply.github.com>
2025-07-17 20:48:56 +0530

    update

---
+++ js/day9/workingwithjson.js
@@ -0,0 +1,26 @@
+// const person = {
+//   name: "Alice",
+//   age: 25,
+// };
+
+
+
+// const jsonstring = JSON.stringify(person);
+// console.log(jsonstring);
+
+
+
+// const obj = JSON.parse(jsonstring)
+
+// console.log(obj);
+// console.log(obj.name);
+
+
+
+
+// function helloo() {
+//   return <h1>Hello, ar </h1>
+// }
+
+
+// helloo()
